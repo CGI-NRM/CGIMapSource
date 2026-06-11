12b9caaf28fc74156e63f3aa8de8a6aea4d073f0
wfenglund <w.f.englund@gmail.com>
2026-06-11 12:33:16 +0200

    added map function to map source code

--- cgi_map_source.r
+++ cgi_map_source.r
@@ -11,3 +11,25 @@ LoadCoordinates <- function(coordinateXLSX) {
   mapData$Longitude <- as.numeric(mapData$Longitude)
   return(mapData)
 }
+
+GenerateMap <- function(mapData) {
+  colorByResultat <- colorFactor(palette = c("darkgreen", "red", "orange"), domain = c("positiv", "negativ", "okänd"), ordered = TRUE)
+
+  counts_html <- paste0(
+    '<div style="background:white; padding:10px;">',
+    'Antal positiva prover: <span style="float:right; font-weight:bold"> ', sum(bisamData$Resultat == "positiv", na.rm = TRUE), "</span><br>",
+    'Antal negativa prover: <span style="float:right; font-weight:bold"> ', sum(bisamData$Resultat == "negativ", na.rm = TRUE), "</span>"
+  )
+  
+  leaflet::leaflet(mapData, elementId = "myMap") %>%
+    leaflet::addTiles() %>%
+    # leaflet::addMarkers(lng = ~ Longitude, lat = ~ Lattitude, label = ~ Prov) %>%
+    leaflet::addCircleMarkers(lng = ~ Longitude, lat = ~ Latitude, popup = ~ paste0("<b>", Prov, "</b>: ", Resultat, " ", Replikat), color = ~ colorByResultat(Resultat)) %>%
+    leaflet::addLegend(values = ~ Resultat, pal = colorByResultat, title = "Resultat") %>%
+    leaflet::addControl(html = counts_html, position = "bottomright")%>%
+    htmlwidgets::onRender("
+      function(el, x) {
+        window.myNativeMap = this; 
+      }
+    ") # expose the map globally
+}

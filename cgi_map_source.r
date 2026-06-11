82bd3a8c58ef3e78a159ee00c1327c349b6050e6
wfenglund <w.f.englund@gmail.com>
2026-06-11 12:42:10 +0200

    added map function to generate table

--- cgi_map_source.r
+++ cgi_map_source.r
@@ -23,7 +23,6 @@ GenerateMap <- function(mapData) {
   
   leaflet::leaflet(mapData, elementId = "myMap") %>%
     leaflet::addTiles() %>%
-    # leaflet::addMarkers(lng = ~ Longitude, lat = ~ Lattitude, label = ~ Prov) %>%
     leaflet::addCircleMarkers(lng = ~ Longitude, lat = ~ Latitude, popup = ~ paste0("<b>", Prov, "</b>: ", Resultat, " ", Replikat), color = ~ colorByResultat(Resultat)) %>%
     leaflet::addLegend(values = ~ Resultat, pal = colorByResultat, title = "Resultat") %>%
     leaflet::addControl(html = counts_html, position = "bottomright")%>%
@@ -32,4 +31,52 @@ GenerateMap <- function(mapData) {
         window.myNativeMap = this; 
       }
     ") # expose the map globally
+}
+
+GenerateTable <- function(mapData) {
+  DT::datatable(
+    data = bisamData, 
+    rownames = FALSE, 
+    options = list(
+      pageLength = 100, 
+      dom = "t",
+      autoWidth = TRUE,
+      columnDefs = list(list(visible = FALSE, targets = c(1, 2))),
+      rowCallback = JS("
+      function(row, data, index) {
+        // data[0]=Prov, data[1]=Latitude, data[2]=Longitude, data[3]=Resultat, data[4]=Replikat
+        $(row).attr('data-lat', data[1]);
+        $(row).attr('data-lng', data[2]);
+        $(row).attr('data-title', data[0]);
+        $(row).attr('data-replicates', data[4]);
+        $(row).css('cursor', 'pointer'); 
+      }
+    ") # store the data in the html
+    ),
+    callback = JS("
+    // Listen for direct clicks on the table body rows
+    table.on('click', 'tbody tr', function() {
+      var row = $(this);
+      var lat   = parseFloat(row.attr('data-lat'));
+      var lng   = parseFloat(row.attr('data-lng'));
+      var title = row.attr('data-title');
+      var date  = row.attr('data-replicates');
+
+      // Check if our global map instance has loaded and coordinates are valid numbers
+      if (window.myNativeMap && !isNaN(lat) && !isNaN(lng)) {
+        
+        window.myNativeMap.closePopup();
+
+        // Target the native Leaflet map engine directly to execute the panning camera 
+        window.myNativeMap.flyTo([lat, lng], 11);
+
+        // Render an interactive info popup window bubble
+        L.popup()
+          .setLatLng([lat, lng])
+          .setContent('<strong>' + title + '</strong><br>' + date)
+          .openOn(window.myNativeMap);
+      }
+    });
+  ")
+  )
 }
